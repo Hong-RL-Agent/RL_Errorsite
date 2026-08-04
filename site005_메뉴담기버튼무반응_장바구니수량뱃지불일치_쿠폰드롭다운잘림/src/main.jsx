5f38b549bb78122d2565eb32d071b322cba84c17
sosoo208 <sohyun2645@naver.com>
2026-08-04 11:05:23 +0900

    revert: 머지된 취약점 및 보안 에러 사이트 제거

--- site005_메뉴담기버튼무반응_장바구니수량뱃지불일치_쿠폰드롭다운잘림/src/main.jsx
+++ site005_메뉴담기버튼무반응_장바구니수량뱃지불일치_쿠폰드롭다운잘림/src/main.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/main.css'
-import { installMockApi } from './mockApi.js'
-
-if (import.meta.env.PROD) {
-  installMockApi()
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
